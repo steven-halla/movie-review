fe18802fb2fca0e6840de875c47e25a0f05ff581
Steven Halla <steven.r.halla@gmail.com>
2021-03-12 22:02:38 -0800

    "starting login/reg validations"

--- src/views/SigninView.jsx
+++ src/views/SigninView.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, {useState, useRef} from "react";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
+import {isEmail} from "validator";
 
-import AuthService from "../services/auth.service";
+
+// import {} from "../services/auth.service";
 import {Link} from 'react-router-dom';
 
 import {signin} from "../services/auth.service";
@@ -11,24 +13,51 @@ import {history} from '../browserHistory';
 
 // import AuthService from "../services/auth.service";
 
+const required = value => {
+    if (!value) {
+        return (
+            <div>
+                <p>This field is required </p>
+            </div>
+        );
+        return;
+    }
+};
 
+const validEmail = (value) => {
+    if (!isEmail(value)) {
+        return (
+            <div>
+                <p>This is not a valid email</p>
+            </div>
+        );
+        return;
+    }
+};
+
+const validPassword = (value) => {
+    if (value.length < 8 || value.length > 20) {
+        return (
+            <div>
+                <p>Password must be between 8 and 20 characters</p>
+            </div>
+        );
+        return;
+    }
+};
 
 export const SigninView = props => {
-
-    const required = (props) => {
-        if (!props) {
-            return(
-                <div className="alert-danger" role="alert">
-                    <p>This Field is required</p>
-                </div>
-            )
-        }
-    }
     const {user, setUser} = props;
 
-    const form =
+    const form = useRef();
+    const checkBtn = useRef();
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState("");
+
+    const [email, setEmail] = useState("");
     const [emailError, setEmailError] = useState("");
     const [passwordError, setPasswordError] = useState("");
+
 
     const onChange = event => {
         console.log('event.target.value =', event.target.value)
@@ -64,43 +93,84 @@ export const SigninView = props => {
         }
     };
 
-    const handleSignin = () => {
-        signin(user.email, user.password).then(response => {
-            // check response for error;
-            history.push("/");
-        });
+    const handleSignin2 = (event) => {
+        event.preventDefault();
+        setMessage("");
+        form.current.validateAll();
+
+    }
+
+    const handleSignin = (event) => {
+        event.preventDefault();
+        setMessage("");
+        setLoading(true);
+
+        form.current.validateAll();
+
+        if (checkBtn.current.context._errors.length === 0) {
+            signin(user.email, user.password).then(response => {
+                // check response for error;
+                history.push("/");
+            }, (error) => {
+                const resMessage =
+                    (error.response && error.response.date &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+
+                setLoading(false);
+                setMessage(resMessage);
+            });
+        }
 
         // axios.get("http://localhost:7777/)", user);
-    };
+    }
 
     return (
         <div>
+
             <div className="loginbgcolor">
                 <h1>Sign In</h1>
             </div>
 
             <div className="loginbox">
-                <div>
-                    <label htmlFor="email">Email: </label>
-                    <input type="text" name="email" onChange={onChangeEmail}/>
-                    <p>{emailError}</p>
-                </div>
+                <Form onSubmit={handleSignin} ref={form}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email: </label>
+                        {/*value={email} we took this out from inline input tag*/}
+                        <Input type="text" name="email" onChange={onChangeEmail} className="form-control"
+                               validations={[required, validEmail]}/>
+                        <p>{emailError}</p>
+                    </div>
 
-                <div>
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" onChange={onChangePassword}/>
-                    <p> {passwordError}</p>
-                </div>
+                    <div className="for/?m-group">
+                        <label htmlFor="password">Password: </label>
+                        <input type="password" name="password" className="form-control" onChange={onChangePassword} r/>
+                        <p> {passwordError}</p>
+                    </div>
 
-                <button onClick={handleSignin}>
-                    Sign In
-                </button>
+                    <div className="form-group">
+                        <button className="btn btn-primary btn-block" disabled={loading}>
+                            {loading && (
+                                <span className="spinner-border spinner-border-sm"></span>
+                            )}
+                            <span>Sign In</span>
+                        </button>
+                    </div>
+
+                    {message && (
+                        <div className="form-group">
+                            <div className="alert alert-danger" role="alert">
+                                {message}
+                            </div>
+                        </div>
+                    )}
+                    <CheckButton style={{display: "none"}} ref={checkBtn}/>
+                </Form>
             </div>
         </div>
-    );
-    return(
-        <div className="alert-danger" role="alert">
-            <p>This Field is required</p>
-        </div>
     )
-}
+
+};
+
+
--- src/views/Validations.jsx
+++ src/views/Validations.jsx
@@ -1,55 +1,55 @@
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
+// import Form from "react-validation/build/form";
+// import Input from "react-validation/build/input";
+// import CheckButton from "react-validation/build/button";
+//
+// import { isEmail } from "validator";
+//
+// const required = value => {
+//     if(!value) {
+//         return(
+//             <div className="alert-danger" role="alert">
+//                 <p>This field is required</p>
+//             </div>
+//         );
+//     }
+// };
+//
+// const email = value => {
+//     if (!isEmail(value)) {
+//         return(
+//             <div className="alert-danger" role="alert">
+//                 <p>This is not a valid email</p>
+//             </div>
+//         );
+//     }
+// };
 
-import { isEmail } from "validator";
-
-const required = value => {
-    if(!value) {
-        return(
-            <div className="alert-danger" role="alert">
-                <p>This field is required</p>
-            </div>
-        );
-    }
-};
-
-const email = value => {
-    if (!isEmail(value)) {
-        return(
-            <div className="alert-danger" role="alert">
-                <p>This is not a valid email</p>
-            </div>
-        );
-    }
-};
-
-render()
-{
-    return (
-...
-    <Form
-        onSubmit={handleLogin}
-        ref={form}
-    >
-        ...
-        <Input
-            type="text"
-            className="form-control"
-        ...
-        validations={[required, email]}
-    />
-
-    <CheckButton
-        style={{ display: "none" }}
-        ref={checkBtn}
-    />
-</Form>
-...
-);
-}
-
-
-    </Form>
-    )
-}
+// render()
+// {
+//     return (
+// ...
+//     <Form
+//         onSubmit={handleLogin}
+//         ref={form}
+//     >
+//         ...
+//         <Input
+//             type="text"
+//             className="form-control"
+//         ...
+//         validations={[required, email]}
+//     />
+//
+//     <CheckButton
+//         style={{ display: "none" }}
+//         ref={checkBtn}
+//     />
+// </Form>
+// ...
+// );
+// }
+//
+//
+//     </Form>
+//     )
+// }
